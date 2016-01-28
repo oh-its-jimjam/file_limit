e94da9a60d7bfaa014ae9be18b28adfcb87cbe58
James Perry <james.austin.perry@gmail.com>
2016-01-28 12:28:34 +0000

    importing v0.1

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# file_limit
-Rust utility functions pertaining to OS file limits
+# limits
+
+A utility library to retrieve and set the file limits for unix-based processes. "Everything is a file"; thus a file can represent network sockets, pipes, file on a file-system, etc.
+Resource-intensive applications typically need to increase the default limit to the maximum. It is also beneficial for low-latency applications to increase the file limit to the maximum number and pre-allocate a data structure to hold the FDs to minimize memory allocations. 
+
+## Get the current file limit
+
+```
+let cur_limit: usize = filelimit::get();
+```
+
+## Get the number of maximum files that can be opened
+
+```
+let max_limit = filelimit::max();
+match max_limit {
+  Limit::Val(v) => ..., //v is the upper bound represented as a usize
+  Limit::Inf => ..., //infinity repesents that there is no upper bound
+}
+```
+
+## Set the file limit to the maximum upper-bound
+
+```
+let new_lim = filelimit::set_to_max().unwrap();
+```
+
+If there is no upper-bound (set to infinity) then it will scale it by factor of 8.
+
+## To-do list
+
+1. Add support for Windows since it uses `HANDLE` as the equivalent of a unix file.
+
+I welcome PRs to add support for Windows.
---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+extern crate libc;
+
+use std::io::{Error, Result};
+
+fn get_limit() -> Result<libc::rlimit> {
+    unsafe {
+        let mut rlim = libc::rlimit{rlim_cur: 0, rlim_max: 0}; 
+        if libc::getrlimit(libc::RLIMIT_NOFILE, &mut rlim) != 0 {
+            return Err(Error::last_os_error());
+        }
+        return Ok(rlim)
+    }
+}
+
+pub fn get() -> Result<usize> {
+    match get_limit() {
+        Ok(rlim) => Ok(rlim.rlim_cur as usize), 
+        Err(err) => Err(err),
+    }
+}
+
+pub enum MaxLimit {
+    Val(usize),
+    NoLimit,
+}
+
+pub fn max() -> Result<MaxLimit> {
+    let rlim = match get_limit() {
+        Ok(rlim) => rlim,
+        Err(err) => return Err(err),
+    };
+    if rlim.rlim_max == libc::RLIM_INFINITY {
+        Ok( MaxLimit::NoLimit )
+    } else {
+        Ok( MaxLimit::Val(rlim.rlim_max as usize))
+    }
+}
+
+pub fn set_to_max() -> Result<usize> {
+    let mut r = match get_limit() {
+        Ok(l) => l,
+        Err(err) => return Err(err),
+    };
+    if r.rlim_max != r.rlim_cur {
+        if r.rlim_max == libc::RLIM_INFINITY { // no limit restriction
+            r.rlim_cur *= 8;
+        } else if r.rlim_cur < r.rlim_max{
+            r.rlim_cur = r.rlim_max;
+        }
+        unsafe {
+            if libc::setrlimit(libc::RLIMIT_NOFILE, &r) < 0 {
+                let err = Error::last_os_error();
+                return Err(err);
+            }
+        }
+    }
+
+    Ok(r.rlim_cur as usize)
+}
+
+#[test]
+fn get_file_limit() {
+    let lim = get().unwrap();
+    assert!(lim > 0);
+}
+
+#[test]
+fn max_limit() {
+    match max() {
+        Ok(MaxLimit::Val(v)) => {
+            assert!(v > 0);
+        },
+        Ok(MaxLimit::NoLimit) => assert!(true),
+        Err(err) => assert!(false),
+    }
+}
+
+#[test]
+fn set_max() {
+    let cur_limit: usize = get().unwrap();
+    let max_limit = set_to_max().unwrap();
+    let new_limit: usize = get().unwrap();
+    assert_eq!(max_limit, new_limit);
+    assert!(new_limit >= cur_limit);
+}
